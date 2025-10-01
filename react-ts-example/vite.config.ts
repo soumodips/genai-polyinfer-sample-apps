8b9644fbf8729bca719ddfc97dbd02f05b893880
soumodips <smdpsinha@gmail.com>
2025-10-02 04:03:56 +0530

    Update genai-polyinfer to version 1.0.6 in all examples

--- react-ts-example/vite.config.ts
+++ react-ts-example/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['node-fetch']
+  },
+  build: {
+    rollupOptions: {
+      external: (id) => id === 'node-fetch'
+    }
+  }
 })
